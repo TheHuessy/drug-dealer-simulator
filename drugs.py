bfc2e3c7f7ad2d4f4606892cc4c88d87add2a81f
TheHuessy <jameshuessy@gmail.com>
2021-09-05 23:15:48 -0400

    quantities in market

--- drugs.py
+++ drugs.py
@@ -14,61 +14,71 @@ drug_data = {
                     "min_rep": 10,
                     "max_rep": 100,
                     "floor_price": 15,
-                    "max_price": 50
+                    "max_price": 50,
+                    "base_quantity": 10
                     },
                 "Speed": {
                     "min_rep": 10,
                     "max_rep": 100,
                     "floor_price": 25,
-                    "max_price": 50
+                    "max_price": 50,
+                    "base_quantity": 15
                     },
                 "Weed": {
                     "min_rep": 0,
                     "max_rep": 100,
                     "floor_price": 5,
-                    "max_price": 60
+                    "max_price": 60,
+                    "base_quantity": 20
                     },
                 "Coke": {
                     "min_rep": 35,
                     "max_rep": 100,
                     "floor_price": 50,
-                    "max_price": 100
+                    "max_price": 100,
+                    "base_quantity": 10
                     },
                 "Shrooms": {
                     "min_rep": 0,
                     "max_rep": 70,
                     "floor_price": 10,
-                    "max_price": 50
+                    "max_price": 50,
+                    "base_quantity": 15
                     },
                 "Aderall": {
                     "min_rep": 50,
                     "max_rep": 100,
                     "floor_price": 50,
-                    "max_price": 75
+                    "max_price": 75,
+                    "base_quantity": 10
                     },
                 "Molly": {
                     "min_rep": 50,
                     "max_rep": 100,
                     "floor_price": 30,
-                    "max_price": 70
+                    "max_price": 70,
+                    "base_quantity": 7
                     },
                 "Meth": {
                     "min_rep": 0,
                     "max_rep": 25,
                     "floor_price": 15,
-                    "max_price": 75
+                    "max_price": 75,
+                    "base_quantity": 20
                     },
                 "Glue": {
                     "min_rep": 0,
                     "max_rep": 15,
                     "floor_price": 2,
-                    "max_price": 10
+                    "max_price": 10,
+                    "base_quantity": 50
                     },
                 "Heroin": {
                     "min_rep": 0,
                     "max_rep": 90,
                     "floor_price": 10,
-                    "max_price": 60
+                    "max_price": 60,
+                    "base_quantity": 10
                     }
                 }
 
@@ -90,6 +100,10 @@ class Drug:
     def _get_drug_price(self):
         self.current_cost = random.choice(range(self.floor_price, self.max_price+1))
 
+#    def _get_drug_quantity(self):
+#        self.quantity = #How to gauge quantity based on no neighborhood data?    ###random.choice(range(self.floor_price, self.max_price+1))
+
+
     def __init__(self, drug_type, value=None):
         if not check_drug_type(drug_type):
             raise NameError("{} is not a valid drug type".format(drug_type))
@@ -107,5 +121,5 @@ class Drug:
             setattr(Drug, key, drug_metadata[key])
 
         ## Generate 'current_cost' value
-        self.get_drug_price()
+        self._get_drug_price()
 
--- neighborhood.py
+++ neighborhood.py
@@ -5,33 +5,50 @@ from random import randrange, sample, choice
 
 hood_data = {
         "Jamaica Plain": {
-            "reputation": 65
+            "reputation": 65,
+            "supply_network": 3
             },
         "Downtown": {
-            "reputation": 95
+            "reputation": 95,
+            "supply_network": 5
             },
         "Back Bay": {
-            "reputation": 80
+            "reputation": 80,
+            "supply_network": 3
             },
         "Fenway": {
-            "reputation": 70
+            "reputation": 70,
+            "supply_network": 4
             },
         "Mission Hill": {
-            "reputation": 50
+            "reputation": 50,
+            "supply_network": 5
             },
         "The Burbs": {
-            "reputation": 100
+            "reputation": 100,
+            "supply_network": 2
             },
         "Roxbury": {
-            "reputation": 50
+            "reputation": 50,
+            "supply_network": 5
             },
         "Mattapan": {
-            "reputation": 60
+            "reputation": 60,
+            "supply_network": 5
             },
         "Southie": {
-            "reputation": 30
+            "reputation": 30,
+            "supply_network": 5
             }
         }
+
+def get_rep_impact_factor(reputation):
+    if reputation <= 60:
+        return(1)
+    elif reputation < 90:
+        return(-1)
+    elif reputation <=100:
+        return(1)
 
 def check_neighborhood_name(neighborhood_name):
     global hood_data
@@ -40,13 +57,19 @@ def check_neighborhood_name(neighborhood_name):
     return(True)
 
 class Market:
-                                                                                                                                                def _drug_numbers(self, event=None):
+
+    def _drug_numbers(self, event=None):
         if not event:
             return(randrange(4,7,1))
         ## Possible place for event logic
+    def _get_rep(self, neighborhood_name):
+        return(hood_data[neighborhood_name]['reputation'])
+
+    def _get_sup_net(self, neighborhood_name):
+        return(hood_data[neighborhood_name]['supply_network'])
 
     def _get_drugs_list(self, neighborhood_name):
-        rep = hood_data[neighborhood_name]['reputation']
+        rep = self._get_rep(neighborhood_name)
         drugs_list = [x for x in drug_data.keys() if drug_data[x]['max_rep'] >= rep and drug_data[x]['min_rep'] <= rep]
         return(drugs_list)
 
@@ -54,15 +77,43 @@ class Market:
         drugs = [Drug(drug) for drug in drugs_list]
         return(drugs)
 
+    def _get_quantities(self, neighborhood_name):
+        rep = self._get_rep(neighborhood_name)
+        rep_index = get_rep_impact_vector(rep)
+        supply_network = self._get_sup_net(neighborhood_name)
+
+
+        for drug_idx in range(len(self.drugs)):
+            base = self.drugs[drug_idx][list(self.drugs[drug_idx].keys())[0]]['base_quantity']
+            price = self.drugs[drug_idx][list(self.drugs[drug_idx].keys())[0]]['current_cost']
+           # base +(2x supply_network) +(rep_index*(0.4*rep)) +(rep*(1/price))
+            quant_calc = (base + (rep_index*(0.4*rep)) + (rep*(1/price)) * (supply_netowrk))
+            self.drugs[drug_idx]][list(self.drugs[drug_idx].keys())[0]]['quantity'] = quant_calc
+
+
+
+
+        
+        ## How to decide the quantities?
+            ## We should use prices and reputation to decide quantity
+                ## Lower prices should mean more quantity
+                ## Reputation could mean more quantity because the neighborhood has a better reuputation, but also it could mean the opposite because swankier neighborhoods might have supply issues over a more seedy neighborhood
+                    ## Maybe create a band system where a reputation range of X means more quantity and Y less quantity and Z more or quantity, sor something like that
+            ## Two multipliers: Reputation/price index, supply_network
+            ## Multiply that by a base quantity level
+                ## This should be new value
+
 
     def __init__(self, neighborhood_name, event=None):
         ## Pick X number of drugs
         number_of_drugs = self._drug_numbers(event)
         ## Get list of drugs based on neighborhood reputation
         drugs_list = self._get_drugs_list(neighborhood_name)
+        
         self.drugs = self._get_drugs(drugs_list)
         ## Generate the different drug objects and save as list[?]
             ## Make it an attribute like drugs or soemthing
+        self._get_quantities(neighborhood_name)
 
 
 
